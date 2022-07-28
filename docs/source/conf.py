8f8aa7cb1ff1bf687485aea78cc90f1be21c9840
Matheus Scaketti <mts.scaketti@gmail.com>
2022-07-28 12:49:41 -0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SaSii'
+copyright = '2021, Scaketti'
+author = 'Matheus'
 
 release = '0.1'
 version = '0.1.0'
